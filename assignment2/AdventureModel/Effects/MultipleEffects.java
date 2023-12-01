9c1f845c73637286e2c9088361db97535785f4d4
EdwardLiono <40928592+EdwardLiono@users.noreply.github.com>
2023-11-30 22:36:09 -0500

    Implemented an effect that unhides all object effect description. Implemented an effect that can give the player items.

--- assignment2/AdventureModel/Effects/MultipleEffects.java
+++ assignment2/AdventureModel/Effects/MultipleEffects.java
@@ -6,6 +6,7 @@ import javax.lang.model.type.NullType;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -29,9 +30,9 @@ public class MultipleEffects implements EffectDecorators{
     }
 
     @Override
-    public void applyFunction(Function<EffectStrategy, NullType> function) {
+    public void applyFunction(Consumer<EffectStrategy> function) {
         for (EffectStrategy effect:effects){
-            function.apply(effect);
+            function.accept(effect);
             if (effect instanceof EffectDecorators){
                 ((EffectDecorators) effect).applyFunction(function);
             }
